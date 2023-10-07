b8f126e25d9175e28263bae78e8b02da7ad0e5e7
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-10-07 11:14:28 +0200

    Improve design of home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,33 +1,35 @@
-import {NgModule, isDevMode} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {FooterComponent} from './footer/footer.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
-import {SustainabilityComponent} from './sustainability/sustainability.component';
-import {AwardsComponent} from './awards/awards.component';
-import {AnalysisComponent} from './analysis/analysis.component';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import {MatButtonModule} from "@angular/material/button";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import {HomeComponent} from './home/home.component';
-import {NavigationComponent} from './navigation/navigation.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from "@angular/material/menu";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SustainabilityComponent } from './sustainability/sustainability.component';
+import { AwardsComponent } from './awards/awards.component';
+import { AnalysisComponent } from './analysis/analysis.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from "@angular/material/menu";
 import { WelcomeComponent } from './welcome/welcome.component';
-import {MatInputModule} from "@angular/material/input";
-import {HttpClientModule} from "@angular/common/http";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatInputModule } from "@angular/material/input";
+import { HttpClientModule } from "@angular/common/http";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 @NgModule({
@@ -67,6 +69,8 @@ import {MatNativeDateModule} from '@angular/material/core';
     MatDatepickerModule,
     MatNativeDateModule,
     FormsModule,
+    MatCardModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
